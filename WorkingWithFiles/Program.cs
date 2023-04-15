169d24ed4384a8fef0d1b8b03839e0b96972c181
tanvircse123 <tanvir.cse.cuet@gmail.com>
2023-04-15 07:41:28 +0600

    added

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -1,31 +1,25 @@
-using WorkingWithFiles;
-var command = args[0];
-if (command == "--file"){
-	var filepath = args[1];
-	// checking if the path fully qualified
-	if(!Path.IsPathFullyQualified(filepath)){
-		Console.WriteLine($"ERROR: path {filepath} is not fully Qualified");
-		Console.ReadLine();
-		return;
-	}
+﻿using WorkingWithFiles;
 
 
-	Console.WriteLine($"Single {filepath} File Selected");
-	ProcessSingleFile(filepath);
-}
-else if(command == "--dir"){
-	var directoryPath = args[1];
-	var filetype = args[2];
-	Console.WriteLine($"Directory {directoryPath} selected for {filetype} files");
-	ProcessDirectory(directoryPath,filetype);
-}else{
-	Console.WriteLine("Invalid Command line options");
-	Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+static void ProcessSingleFile(string filepath){
+	var fileProcessor = new FileProcessor(filepath);
+	fileProcessor.Process();
 }
 
-void ProcessDirectory(string directoryPath, string filetype)
-{
-    // get all the file of a spexfic extension
+static void ProcessDirectory(string directoryPath,string filetype){
+	 // get all the file of a spexfic extension
 	switch(filetype)
 	{
 		case "TEXT":
@@ -41,9 +35,3 @@ void ProcessDirectory(string directoryPath, string filetype)
 			return;
 	}
 }
-
-static void ProcessSingleFile(string filepath){
-	var fileProcessor = new FileProcessor(filepath);
-	fileProcessor.Process();
-}
-
