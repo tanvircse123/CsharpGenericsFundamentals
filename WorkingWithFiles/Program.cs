e8a9833bdcb9c7e0972b5b7ccc67c06f121034e0
tanvircse123 <tanvir.cse.cuet@gmail.com>
2023-04-15 07:37:23 +0600

    added

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -1,4 +1,4 @@
-﻿using WorkingWithFiles;
+using WorkingWithFiles;
 var command = args[0];
 if (command == "--file"){
 	var filepath = args[1];
@@ -17,18 +17,33 @@ else if(command == "--dir"){
 	var directoryPath = args[1];
 	var filetype = args[2];
 	Console.WriteLine($"Directory {directoryPath} selected for {filetype} files");
-	//ProcessDirectory(directoryPath,filetype);
+	ProcessDirectory(directoryPath,filetype);
 }else{
 	Console.WriteLine("Invalid Command line options");
 	Console.ReadLine();
 }
 
+void ProcessDirectory(string directoryPath, string filetype)
+{
+    // get all the file of a spexfic extension
+	switch(filetype)
+	{
+		case "TEXT":
+		   var textfiles = Directory.GetFiles(directoryPath,"*.txt");
+		   foreach(var itempath in textfiles){
+				var fileprocessor = new FileProcessor(itempath);
+				fileprocessor.Process();
+
+		   }
+		   break;
+		default:
+			Console.WriteLine($"ERROR: {filetype} is not supported");
+			return;
+	}
+}
 
 static void ProcessSingleFile(string filepath){
 	var fileProcessor = new FileProcessor(filepath);
 	fileProcessor.Process();
 }
 
-static void ProcessDirectory(){
-
-}
