8fb2561c61cfd27125519e638f479f64036bd9d9
tanvircse123 <tanvir.cse.cuet@gmail.com>
2023-04-15 07:42:29 +0600

    added

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -1,34 +1,24 @@
 ﻿using WorkingWithFiles;
-var command = args[0];
-if (command == "--file"){
-	var filepath = args[1];
-	// checking if the path fully qualified
-	if(!Path.IsPathFullyQualified(filepath)){
-		Console.WriteLine($"ERROR: path {filepath} is not fully Qualified");
-		Console.ReadLine();
-		return;
-	}
-
-
-	Console.WriteLine($"Single {filepath} File Selected");
-	ProcessSingleFile(filepath);
-}
-else if(command == "--dir"){
-	var directoryPath = args[1];
-	var filetype = args[2];
-	Console.WriteLine($"Directory {directoryPath} selected for {filetype} files");
-	//ProcessDirectory(directoryPath,filetype);
-}else{
-	Console.WriteLine("Invalid Command line options");
-	Console.ReadLine();
-}
-
 
 static void ProcessSingleFile(string filepath){
 	var fileProcessor = new FileProcessor(filepath);
 	fileProcessor.Process();
 }
 
-static void ProcessDirectory(){
+static void ProcessDirectory(string directoryPath, string filetype){
+ // get all the file of a spexfic extension
+	switch(filetype)
+	{
+		case "TEXT":
+		   var textfiles = Directory.GetFiles(directoryPath,"*.txt");
+		   foreach(var itempath in textfiles){
+				var fileprocessor = new FileProcessor(itempath);
+				fileprocessor.Process();
 
+		   }
+		   break;
+		default:
+			Console.WriteLine($"ERROR: {filetype} is not supported");
+			return;
+	}
 }
